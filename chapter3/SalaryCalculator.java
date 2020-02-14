335c648d4108735722901026c60eeaf3e6d9a3d6
rebecca23williams <47248246+rebecca23williams@users.noreply.github.com>
2020-02-14 09:14:53 +0000

    Create SalaryCalculator.java

---
+++ chapter3/SalaryCalculator.java
@@ -0,0 +1,29 @@
+package chapter3;
+
+/* IF Statement.
+All salespeople get a payment of $1000 a week.
+Salespeople who exceed 10 sales get an additional bonus of $250
+ */
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args){
+
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+        System.out.println("How many sales did the employee make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        if(sales > quota){
+            salary = salary + bonus;
+        }
+
+        System.out.println("The employee's pay is $" + salary);
+    }
+}
