7d528925a706f9d560465353a28d8951e073c32f
rebecca23williams <47248246+rebecca23williams@users.noreply.github.com>
2020-02-18 16:56:02 +0000

    Create ExceptionHandling.java

---
+++ chapter13/ExceptionHandling.java
@@ -0,0 +1,41 @@
+package chapter13;
+
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+    public static void main(String args[]){
+        //createNewFile();
+        numbersExceptionHandling();
+    }
+
+    public static void createNewFile(){
+        File file = new File("resources/nonexistant.txt");
+        try{
+            file.createNewFile();
+        } catch (java.io.IOException e){
+            System.out.println("Directory does not exist");
+            e.printStackTrace();
+        }
+    }
+    public static void numbersExceptionHandling(){
+        File file = new File("resources/numbers.txt");
+        Scanner fileReader = null;
+        try{
+            fileReader = new Scanner(file);
+
+            while (fileReader.hasNext()){
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        }catch (java.io.FileNotFoundException | InputMismatchException ex){
+            ex.printStackTrace();
+        }finally {
+            fileReader.close();
+        }
+    }
+}
